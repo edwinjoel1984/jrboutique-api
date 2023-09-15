64a784ba6d9bc11d110c454b9557108f06bcb80c
Edwin Ulloa <edwinjoel1984@gmail.com>
2023-09-15 10:01:47 -0500

    add logout method

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -10,32 +10,43 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
-      
-        
+    public function login(Request $request)
+    {
+
+
         $this->validateLogin($request);
 
-        if(Auth::attempt($request->only('email', 'password'))){
+        if (Auth::attempt($request->only('email', 'password'))) {
             $user = $request->user();
             $token =  $user->createToken($request->email)->plainTextToken;
             return response()->json([
-                'token'=>$token,
-                'email'=>$user->email,
-                'name'=>$user->name,
-                'message'=>'Success'
+                'token' => $token,
+                'email' => $user->email,
+                'name' => $user->name,
+                'message' => 'Success'
             ]);
         }
 
         return response()->json([
-            'message'=>'Correo o Contrasena Invalida'
-        ], 401);
+            'message' => 'Correo o Contrasena Invalida'
+        ], 422);
     }
 
-    public function validateLogin(Request $request){
+    public function validateLogin(Request $request)
+    {
         return $request->validate([
-            'email'=>'required|email',
-            'password'=>'required'
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
     }
 
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Sesion finalizada correctamente'
+        ], 200);
+    }
 }
